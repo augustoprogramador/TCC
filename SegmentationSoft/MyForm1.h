30876ecddb77b313fff91d2bd400515e16fd32f3
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2022-08-25 15:48:24 -0300

    ultimas tentativas

--- SegmentationSoft/MyForm1.h
+++ SegmentationSoft/MyForm1.h
@@ -1,5 +1,7 @@
 #pragma once
 
+using namespace System::IO;
+
 namespace SegmentationSoft {
 
 	using namespace System;
@@ -38,8 +40,14 @@ namespace SegmentationSoft {
 	protected:
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::TableLayoutPanel^ tableLayoutPanel1;
-	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ btn_adicionar;
+	private: System::Windows::Forms::Button^ btn_remover;
+
+
+	private: System::Windows::Forms::ListBox^ lb_entrada;
+	private: System::Windows::Forms::ListBox^ lb_saida;
+	private: System::Windows::Forms::SaveFileDialog^ saveFileDialog1;
+	private: System::Windows::Forms::FolderBrowserDialog^ folderBrowserDialog1;
 
 
 
@@ -61,8 +69,12 @@ namespace SegmentationSoft {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->tableLayoutPanel1 = (gcnew System::Windows::Forms::TableLayoutPanel());
-			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->btn_adicionar = (gcnew System::Windows::Forms::Button());
+			this->btn_remover = (gcnew System::Windows::Forms::Button());
+			this->lb_entrada = (gcnew System::Windows::Forms::ListBox());
+			this->lb_saida = (gcnew System::Windows::Forms::ListBox());
+			this->saveFileDialog1 = (gcnew System::Windows::Forms::SaveFileDialog());
+			this->folderBrowserDialog1 = (gcnew System::Windows::Forms::FolderBrowserDialog());
 			this->tableLayoutPanel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -71,7 +83,7 @@ namespace SegmentationSoft {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Consolas", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(47, 25);
+			this->label1->Location = System::Drawing::Point(102, 25);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(94, 24);
 			this->label1->TabIndex = 0;
@@ -82,7 +94,7 @@ namespace SegmentationSoft {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Consolas", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(210, 25);
+			this->label2->Location = System::Drawing::Point(354, 25);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(70, 24);
 			this->label2->TabIndex = 1;
@@ -95,38 +107,59 @@ namespace SegmentationSoft {
 				50)));
 			this->tableLayoutPanel1->ColumnStyles->Add((gcnew System::Windows::Forms::ColumnStyle(System::Windows::Forms::SizeType::Percent,
 				50)));
-			this->tableLayoutPanel1->Controls->Add(this->button1, 0, 0);
-			this->tableLayoutPanel1->Controls->Add(this->button2, 1, 0);
-			this->tableLayoutPanel1->Location = System::Drawing::Point(51, 310);
+			this->tableLayoutPanel1->Controls->Add(this->btn_adicionar, 0, 0);
+			this->tableLayoutPanel1->Controls->Add(this->btn_remover, 1, 0);
+			this->tableLayoutPanel1->Location = System::Drawing::Point(153, 317);
 			this->tableLayoutPanel1->Name = L"tableLayoutPanel1";
 			this->tableLayoutPanel1->RowCount = 1;
 			this->tableLayoutPanel1->RowStyles->Add((gcnew System::Windows::Forms::RowStyle(System::Windows::Forms::SizeType::Percent, 50)));
 			this->tableLayoutPanel1->Size = System::Drawing::Size(229, 53);
 			this->tableLayoutPanel1->TabIndex = 2;
 			// 
-			// button1
+			// btn_adicionar
 			// 
-			this->button1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+			this->btn_adicionar->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->button1->Location = System::Drawing::Point(3, 3);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(108, 47);
-			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
+			this->btn_adicionar->Location = System::Drawing::Point(3, 3);
+			this->btn_adicionar->Name = L"btn_adicionar";
+			this->btn_adicionar->Size = System::Drawing::Size(108, 47);
+			this->btn_adicionar->TabIndex = 0;
+			this->btn_adicionar->Text = L"Adicionar";
+			this->btn_adicionar->UseVisualStyleBackColor = true;
+			this->btn_adicionar->Click += gcnew System::EventHandler(this, &MyForm1::btn_adicionar_Click);
 			// 
-			// button2
+			// btn_remover
 			// 
-			this->button2->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
+			this->btn_remover->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom)
 				| System::Windows::Forms::AnchorStyles::Left)
 				| System::Windows::Forms::AnchorStyles::Right));
-			this->button2->Location = System::Drawing::Point(117, 3);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(109, 47);
-			this->button2->TabIndex = 1;
-			this->button2->Text = L"button2";
-			this->button2->UseVisualStyleBackColor = true;
+			this->btn_remover->Location = System::Drawing::Point(117, 3);
+			this->btn_remover->Name = L"btn_remover";
+			this->btn_remover->Size = System::Drawing::Size(109, 47);
+			this->btn_remover->TabIndex = 1;
+			this->btn_remover->Text = L"Remover";
+			this->btn_remover->UseVisualStyleBackColor = true;
+			// 
+			// lb_entrada
+			// 
+			this->lb_entrada->FormattingEnabled = true;
+			this->lb_entrada->Location = System::Drawing::Point(51, 64);
+			this->lb_entrada->Name = L"lb_entrada";
+			this->lb_entrada->Size = System::Drawing::Size(197, 238);
+			this->lb_entrada->TabIndex = 3;
+			// 
+			// lb_saida
+			// 
+			this->lb_saida->FormattingEnabled = true;
+			this->lb_saida->Location = System::Drawing::Point(291, 64);
+			this->lb_saida->Name = L"lb_saida";
+			this->lb_saida->Size = System::Drawing::Size(197, 238);
+			this->lb_saida->TabIndex = 4;
+			// 
+			// saveFileDialog1
+			// 
+			this->saveFileDialog1->InitialDirectory = L"./";
 			// 
 			// MyForm1
 			// 
@@ -134,6 +167,8 @@ namespace SegmentationSoft {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
 			this->ClientSize = System::Drawing::Size(689, 465);
+			this->Controls->Add(this->lb_saida);
+			this->Controls->Add(this->lb_entrada);
 			this->Controls->Add(this->tableLayoutPanel1);
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->label1);
@@ -150,5 +185,17 @@ namespace SegmentationSoft {
 	
 	private: System::Void MyForm1_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	};
+	private: System::Void btn_adicionar_Click(System::Object^ sender, System::EventArgs^ e) {
+		Stream^ myStream;
+		SaveFileDialog^ saveFileDialog1 = gcnew SaveFileDialog();
+		saveFileDialog1->Filter = "arquivos de imagem |*.jpg|*.bmp|*.png|*.gif";
+		saveFileDialog1->FilterIndex = 2;
+		saveFileDialog1->RestoreDirectory = true;
+		//if (saveFileDialog1->ShowDialog() == System::) {
+			//if ((myStream = saveFileDialog1->OpenFile()) != nullptr) {
+				//myStream->Close();
+			//}
+		//}
+	}
+};
 }
